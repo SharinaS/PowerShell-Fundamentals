3669aace718f183c1dc63b2189f9bd33699272f9
SharinaS <sharina@1strategy.com>
2020-10-09 23:10:12 -0700

    make demo while loop, steps for launching new instances, add sample data to csv

---
+++ aws/launch_instance.ps1
@@ -0,0 +1,6 @@
+# Set AMI id to a variable called ami_id
+# Count # lines (instances) in csv file and set to a variable called instance_numb
+# Make an empty array of length instance_numb called new_instances_array
+# While i is greater than or equal to instance_numb, launch a new instance, store the instane id in the new_instances_array, and reduce count of i by 1. 
+
+
